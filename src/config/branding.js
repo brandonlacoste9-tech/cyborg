1f60bfe9c585fd7f22ddc43dbcc06addcf73f898
Brandon Lacoste <brandonlacoste9@gmail.com>
2026-06-14 13:02:13 -0400

    Fixed title text to CYBORG GAMERS

--- src/config/branding.js
+++ src/config/branding.js
@@ -3,7 +3,7 @@ export const getBranding = () => {
   return {
     name: 'Cyborg Gamers',
     logoText: 'CYBORG GAMERS',
-    heroTitle: 'GAMERS CYBORG',
+    heroTitle: 'CYBORG GAMERS',
     heroSubtitle: 'OVER 800+ OF YOUR FAVOURITE GAMES. ALL IN ONE PLACE. ANYTIME. EVERYWHERE. FREE REGISTRATION. START PLAYING.',
     heroBackground: 'linear-gradient(to right, rgba(10, 10, 10, 0.95) 0%, rgba(10, 10, 10, 0.8) 40%, rgba(10, 10, 10, 0.4) 100%), url(/cyborg-bg.jpg)',
     domain: 'cyborggamers.com',
